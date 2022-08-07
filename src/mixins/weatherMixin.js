c7ec6d8b6439386a860494731309d1b079ad84dc
jyj <busy.joj@gmail.com>
2022-08-07 18:16:48 +0900

    API 보안 작업

--- src/mixins/weatherMixin.js
+++ src/mixins/weatherMixin.js
@@ -6,7 +6,7 @@ const weatherMixin = {
     },
     methods: {
         async getWeatherInfo(city) {
-            const API_KEY = "c737530293546e8a6c1306f5b49dea1c"
+            const API_KEY = process.env.VUE_APP_API_KEY
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`
             const res = await axios.get(url)
             const { main, wind, weather } = res.data
