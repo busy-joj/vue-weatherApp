a66a3f487dc3e661eb66ae6f1c5a72126fe84caf
busy-joj <busy.joj@gmail.com>
2022-10-25 17:13:54 +0900

    콘텐츠 스타일 수정

--- src/components/weather/WeatherItem.vue
+++ src/components/weather/WeatherItem.vue
@@ -1,9 +1,11 @@
 <template>
   <li class="weather-item">
     <h3>{{ weather.label }}</h3>
+    <div class="weather-img">
+        <img :src="weather.icon">
+    </div>
     <p class="temperature">{{ weather.temperature }}&deg;c</p>
     <p class="info">
-        <img :src="weather.icon">
         <span>습도 {{ weather.humidity }}</span>
         <span>바람 {{ weather.wind }}m/s</span>
     </p>
@@ -33,7 +35,9 @@ export default {
 <style scoped>
 .weather-item{display:flex;flex-direction: column;justify-content:center;align-items: center;padding:0.5em;border-radius:20px;box-shadow: 10px 10px 20px #a6a6a6,-10px -10px 20px #ffffff;margin-bottom:20px;}
 .weather-item h3{font-size:14px;color:#333;}
-.weather-item .temperature{font-size:36px;}
+.weather-item .temperature{font-size:30px;}
 .info{display:flex;justify-content:center;align-items: center;font-size:12px;}
 .info *{margin-right:0.4em;}
+.weather-img{width:80px;}
+.weather-img img{width:100%;}
 </style>
